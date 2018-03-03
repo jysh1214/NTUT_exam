48192778a2068e6186e20924c831b5c9a0f53e03
jysh1214 <jyxemperor@gmail.com>
2018-03-03 13:00:18 +0800

    Update 104_07.cpp

--- 104_07.cpp
+++ 104_07.cpp
@@ -31,7 +31,7 @@ class Square: public Rectangle{
     public:
         void setup(int w){width = w;}
         void name(){cout<<"Square"<<endl;}
-        void area(){return (width*width);}
+        int area(){return (width*width);}
         Square(){width = 3;}
 };
 
@@ -43,13 +43,19 @@ int main(){
     Triangle * ptr_polygon2 = triangle;
     Square * ptr_polygon3 = square;
     ptr_polygon1->setup(2,2);
+    cout<<"Problem 7-1: ";
     cout<<ptr_polygon1->area()<<endl;       // Problem 7-1
+    cout<<"Problem 7-2: ";
     cout<<ptr_polygon3->area()<<endl;       // Problem 7-2
     Rectangle * ptr_polygon4 = ptr_polygon3;
     ptr_polygon4->setup(4,4);
+    cout<<"Problem 7-3: ";
     cout<<ptr_polygon4->area()<<endl;       // Problem 7-3
+    cout<<"Problem 7-4: ";
     ptr_polygon2->name();                   // Problem 7-4
+    cout<<"Problem 7-5: ";
     ptr_polygon3->name();                   // Problem 7-5
+    cout<<"Problem 7-6: ";
     ptr_polygon4->name();                   // Problem 7-6
     return 0;
 }
